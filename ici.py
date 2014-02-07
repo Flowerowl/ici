d53a29fcb815f03d55b0097dc9534b7874a9c8df
yuzhe <lazynightz@gmail.com>
2014-02-07 17:09:23 +0800

    add translation func

--- ici.py
+++ ici.py
@@ -1,5 +1,37 @@
+#!/usr/bin/env python
 #encoding:utf-8
-
+import sys
 import urllib2
+import getopt
+from xml.dom import minidom
 
 
+KEY = 'E0F0D336AF47D3797C68372A869BDBC5'
+URL = 'http://dict-co.iciba.com/api/dictionary.php'
+
+def get_response(word):
+    response = urllib2.urlopen(URL + '?key=' + KEY + '&w=' + word)
+    read_xml(response)
+
+def read_xml(xml):
+    dom = minidom.parse(xml)
+    root = dom.documentElement
+    show(root)
+
+def show(node):
+    if not node.hasChildNodes():
+        if node.nodeType == node.TEXT_NODE:
+            if node.data != '\n':
+                print node.data.replace('\n', '')
+    else:
+        for e in node.childNodes:
+            show(e)
+
+if __name__ == '__main__':
+    try:
+        options, args = getopt.getopt(sys.argv[1:], ["help"])
+    except getopt.GetoptError as e:
+        pass
+
+    word = " ".join(args)
+    get_response(word)
